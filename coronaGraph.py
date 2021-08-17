07fd6d4ee5cf53dec5e160fda36a2b849ec19b1d
Shubh Jain <65031443+Shubh-Jxin@users.noreply.github.com>
2021-08-17 19:44:12 +0530

    Delete coronaGraph.py

--- coronaGraph.py
+++
@@ -1,5 +0,0 @@
-import plotly.express as px
-import pandas as pd
-df = pd.read_csv("D:/projects_vansh/module3/PROJ103/data.csv")
-fig = px.line(df,x="country",y = "cases",color = "date",title = "Per Capita Income")
-fig.show
